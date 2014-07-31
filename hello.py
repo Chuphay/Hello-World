3e04f554f4b7a2dac0ca8ee0273649dcbe8b30ff
SplashDance <stevesargentjr@hotmail.com>
2014-07-31 19:26:31 -0400

    PEP8 fixes

--- hello.py
+++ hello.py
@@ -1,7 +1,17 @@
+try:
+    from tkinter import *
+except ImportError:
+    from Tkinter import *
+
+
 def main():
-    print("hello world!")
+    root = Tk()
+
+    w = Label(root, text="Hello, world!")
+    w.pack()
+
+    root.mainloop()
 
 #comments
 if __name__ == '__main__':
     main()
-
