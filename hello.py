aa215de94f0f34ff806a6f091bba9d4e3a6550a9
tp9 <theprofessional9@gmail.com>
2014-07-30 14:49:42 -1000

    Added tkinter

--- hello.py
+++ hello.py
@@ -1,5 +1,12 @@
+from tkinter import *
+
 def main():
-    print("hello world!")
+    root = Tk()
+    
+    w = Label(root, text = "Hello, world!")
+    w.pack()
+    
+    root.mainloop()
 
 #comments
 if __name__ == '__main__': main()
